3062a3cb2b529152c8eef2c7c858f6ca2f35587d
sionverschraege <sion.verschraege@hogent.be>
2024-12-01 10:56:17 +0100

    Added test option

--- code/1.1.py
+++ code/1.1.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+test = False
 path = Path(__file__)
-input = open(f'{path.parent.parent.absolute()}\\input\\{path.name[:-3]}')
+input = open(f'{path.parent.parent.absolute()}\\input\\{path.name[:-3]}{'.test' if test else ''}')
 lines = input.readlines()
 for i in range(len(lines)):
     lines[i] = [int(num) for num in lines[i].removesuffix('\n').split('   ')]
--- code/dummy.py
+++ code/dummy.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+test = True
 path = Path(__file__)
-input = open(f'{path.parent.parent.absolute()}\\input\\{path.name[:-3]}')
+input = open(f'{path.parent.parent.absolute()}\\input\\{path.name[:-3]}{'.test' if test else ''}')
 lines = input.readlines()
 for i in range(len(lines)):
     lines[i] = lines[i].removesuffix('\n')
