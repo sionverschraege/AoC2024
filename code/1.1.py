06684ae4a28dbb88e03043077a43dfc20cac58ec
sionverschraege <sion.verschraege@hogent.be>
2024-12-01 10:49:46 +0100

    Added day 1

---
+++ code/1.1.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+path = Path(__file__)
+input = open(f'{path.parent.parent.absolute()}\\input\\{path.name[:-3]}')
+lines = input.readlines()
+for i in range(len(lines)):
+    lines[i] = [int(num) for num in lines[i].removesuffix('\n').split('   ')]
+
+left = [l[0] for l in lines]
+right = [l[1] for l in lines]
+left.sort()
+right.sort()
+
+diff = 0
+for i in range(len(left)):
+    diff += abs(left[i] - right[i])
+print(diff)
---
+++ code/1.2.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+path = Path(__file__)
+input = open(f'{path.parent.parent.absolute()}\\input\\{path.name[:-3]}')
+lines = input.readlines()
+for i in range(len(lines)):
+    lines[i] = [int(num) for num in lines[i].removesuffix('\n').split('   ')]
+
+left = [l[0] for l in lines]
+right = [l[1] for l in lines]
+
+countr = {}
+for n in right:
+    if n not in countr.keys():
+        countr[n] = 0
+    countr[n] += 1
+
+similarity = 0
+for n in left:
+    if n in countr.keys():
+        similarity += countr[n] * n
+
+print(similarity)
